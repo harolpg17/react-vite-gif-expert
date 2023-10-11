f12ca34c49f8aa9ea6787468e8fbc3d521edce7a
harolpg17 <harolpg17@gmail.com>
2023-10-11 11:20:38 -0500

    Add files

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export const AddCategory = ({setCategories, onNewCategory}) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const newInputValue = inputValue.trim();
+
+        if (newInputValue.length <= 1) return;
+
+        // setCategories(categories => [...categories, inputValue]);
+        onNewCategory(newInputValue);
+        setInputValue('');
+    }
+    
+  return (
+    <form onSubmit={onSubmit}>
+        <input 
+            type="text"
+            placeholder="Buscar gifs"
+            value={inputValue}
+            onChange={onInputChange} />
+    </form>
+  )
+}
